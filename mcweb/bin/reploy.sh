b1dd5c0a647ec1c1e294e03737c4457df6287424
mamemomonga <mamemomonga@gmail.com>
2019-07-08 09:21:19 +0900

    typo

--- mcweb/bin/reploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eux
-BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
-cd $BASEDIR
-
-make
-sudo systemctl restart mamemocraft-web
-exec sudo journalctl -u mamemocraft-web -f
-
